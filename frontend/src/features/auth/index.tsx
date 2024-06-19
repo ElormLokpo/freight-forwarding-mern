9df4d87b5478bf68f9aa6ab02fa88be85056aa8a
ElormLokpo <benedictdev31@gmail.com>
2024-06-19 22:38:46 +0000

    changes

--- frontend/src/features/auth/index.tsx
+++ frontend/src/features/auth/index.tsx
@@ -6,7 +6,7 @@ import SignInForm from './forms/signin-form'
 const AuthPage = () => {
   return (
     <div className='h-screen bg-gray-50 flex justify-center items-center'>
-     <SignInForm />
+     <SignUpForm />
     </div>
   )
 }
